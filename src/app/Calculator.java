c67d6196b2014d004859a63d44fef19064d6e5a9
Karol Programuje <karol341654@gmail.com>
2023-05-22 13:19:26 +0200

    Adding CalculatorGui class, create frame. First app run;

--- src/app/Calculator.java
+++ src/app/Calculator.java
@@ -1,7 +1,9 @@
 package app;
 
+import app.gui.CalculatorGui;
+
 public class Calculator {
     public static void main(String[] args) {
-
+        new CalculatorGui();
     }
 }
---
+++ src/app/gui/CalculatorGui.java
@@ -0,0 +1,16 @@
+package app.gui;
+
+import javax.swing.*;
+
+public class CalculatorGui {
+    public CalculatorGui() {
+        JFrame frame = new JFrame("Calculator");
+        frame.setSize(420, 550);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(null);
+
+
+        frame.setVisible(true);
+    }
+}
