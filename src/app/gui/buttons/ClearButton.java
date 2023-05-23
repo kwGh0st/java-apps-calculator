fe11ea287c4fa10b7e2e90819f1bb84dcc32bf30
Karol Programuje <karol341654@gmail.com>
2023-05-23 10:46:14 +0200

    Creating ClearButton class. Closes #2.

---
+++ src/app/gui/buttons/ClearButton.java
@@ -0,0 +1,9 @@
+package app.gui.buttons;
+
+import javax.swing.*;
+
+public class ClearButton extends AbstractButton {
+    public ClearButton(String text, JTextField textField) {
+        super(text, textField);
+    }
+}
